c6bdb5c97973b033b206d4a498e277c4159578c2
Yudi Candra <yudhichandra02@gmail.com>
2023-04-06 09:58:22 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,13 +13,44 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+
+        $validate = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if ($validate->fails()) {
+
+            return redirect('login')->with(['error' => 'Silahkan isi semua form yang tersedia']);
+        }
+
+        if (!$user) {
+            return redirect('login')->with(['error' => 'Username tidak terdaftar']);
+        }
+
+        $credentials = request(['email', 'password']);
+        if (!Auth::attempt($credentials)) {
+            return redirect('login')->with(['error' => 'Username atau password salah']);
+        }
+
+        if (!Hash::check($request->password, $user->password)) {
+            return redirect('login')->with(['error' => 'Unathorized, password yang kamu masukkan tidak sesuai']);
+        }
+
+
+        return redirect('dashboard');
+    }
     /**
      * Handle an incoming authentication request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function login(Request $request)
+    public function loginApi(Request $request)
     {
         $user = User::where('email', $request->email)->first();
 
--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function () {
     // auth
-    Route::post('auth/login', [AuthController::class, 'login']);
+    Route::post('auth/login', [AuthController::class, 'loginApi']);
     Route::post('auth/register', [AuthController::class, 'register']);
     // get post list
     Route::post('posts', [PostController::class, 'index']);
